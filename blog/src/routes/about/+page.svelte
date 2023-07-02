52b8966e9fe2aebf8f19b3c5b9749cd7922510e2
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-07-01 21:17:27 -0600

    a few edits to about

--- blog/src/routes/about/+page.svelte
+++ blog/src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
 <!-- about/+page.svelte -->
 <h1>About</h1>
 
-<p>This blog went live late June 2023! I initially went with Hugo, then switched to Next.js, and finally decided to go back to what I know (a little bit) which is SvelteKit. I plan on posting notes here from my TryHackMe adventures, some software development eventually. Not here to show the world how rad I am (cause I'm really not 1337), but more like "Dude, this is where I keep my notes." Hoping somebody finds this gong show useful, my way of giving back to the internet. Anywho... Thank you, fellow netizen.</p>
+<p>This blog went live late June 2023! I initially went with Hugo, then switched to Next.js, Jekyll hated me, and finally decided to go back to what I know (a little bit) which is SvelteKit. I plan on posting notes here from my TryHackMe adventures, skript kitty coding with AI, and some software development eventually. Hoping somebody finds this gong show useful, my way of giving back to the internet. Anywho... Thank you, fellow netizen.</p>
