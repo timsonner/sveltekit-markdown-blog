1493be04b5c266383a219a45e6dbb233d6b73c9a
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-07-01 21:52:06 -0600

    better codeblock font color, code comments

--- blog/src/lib/utils/index.js
+++ blog/src/lib/utils/index.js
@@ -1,3 +1,6 @@
+// This whole function is basically dealing with the markdown files on the filesystem.
+// Searches a directory path for files, does a bit of slicing to remove the .md from the filename
+// so we can use it as a post path.
 export const fetchMarkdownPosts = async () => {
     const allPostFiles = import.meta.glob('/src/routes/blog/*.md')
     const iterablePostFiles = Object.entries(allPostFiles)
--- blog/src/routes/blog/+page.js
+++ blog/src/routes/blog/+page.js
@@ -1,4 +1,9 @@
 // src/routes/blog/+page.js
+
+// Server side load function, makes a reuest to the backedn for posts. 
+// Posts are sent to the front via this load function, which +page.svelte runs imediately.
+// Results are a JSON array of post objects.
+// These results will be available to the front (+page.svelte) via the 'data' object.
 export const load = async ({ fetch }) => {
     const response = await fetch(`/api/posts`)
     const posts = await response.json()
