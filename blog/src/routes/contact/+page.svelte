30f86fd9a4a401bef78c628efb63984a0298186e
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2023-07-02 01:15:32 -0600

    fairly satisfied with styles at this point

--- blog/src/routes/contact/+page.svelte
+++ blog/src/routes/contact/+page.svelte
@@ -1,5 +1,15 @@
 <!-- contact/+page.svelte -->
+<body>
 <h1>Contact</h1>
 
 <p><a href="https://www.linkedin.com/in/timsonner/">Hit me up on LinkedIn</a></p>
 <p><a href="https://github.com/timsonner">Or GitHub</a></p>
+</body>
+
+<style>
+    body {
+    padding: 1rem;
+    margin: 2rem auto;
+    max-width: 40rem;
+  }
+</style>
